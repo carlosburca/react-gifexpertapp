73393b7f83caf558ccfec716cbdcd5d5a76bc892
Carlos Burgos <cburgos@abstrahere.com>
2020-09-27 13:59:01 -0300

    docs agregado

---
+++ docs/precache-manifest.7ce1a414f87b53caff622571e7d49abb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "287e12428a1f686c7febccf0760dda5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5917b1d50fec71a7752",
+    "url": "/static/css/main.5fb3b179.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5917b1d50fec71a7752",
+    "url": "/static/js/main.e0b6a12f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
